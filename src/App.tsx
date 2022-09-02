7afd0d65bbd9b0209768a6ebf7637d1ed12e722b
Laurent <magrez0laurent@gmail.com>
2022-09-03 00:05:06 +0200

    feat: drop down button file creation, imported in app for testing purpose

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+import {DropDownButton} from "./components/DropDownButton";
+
 function App() {
   return (
     <>
-      <p className="Text-test">App</p>
+      <DropDownButton />
     </>
   );
 }

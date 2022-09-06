7041c6f38b83dbeb5d9a1ed4778ecac13c768fb5
Thib <clement.thibaut.guillaume@gmail.com>
2022-09-06 17:25:00 +0200

    Features: Created the MatchSelected Component + edition of SCSS file

--- src/pages-layout/GamblePage.tsx
+++ src/pages-layout/GamblePage.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import MatchSelected from "../components/MatchSelected";
 
 export const GamblePage = () => {
-  return <div>GamblePage</div>;
+
+  const data = {
+    gameImg: "https://www.pedagojeux.fr/wp-content/uploads/2019/11/1280x720_LoL.jpg",
+    game: "League of Legends",
+    league: "pupuLeague",
+    date: "24/06/22",
+    time: "20:00",
+    teamA: "Rogue",
+    teamB: "MAD Lions"
+  }
+
+  return (
+    <>
+      <MatchSelected data={data}/>
+    </>
+  );
 };

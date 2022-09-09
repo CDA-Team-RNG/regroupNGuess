b9ded4d22db3462c5645a710370f0c4de4a93b3d
Haddouche Rym <rymmihoub@gmail.com>
2022-09-09 11:57:47 +0200

    correction of component gamblePage

--- src/pages-layout/GamblePage.tsx
+++ src/pages-layout/GamblePage.tsx
@@ -1,19 +1,9 @@
 import React from "react";
-import { Match } from "../components/Match";
+
 
 export const GamblePage = () => {
   return (
-    <>
-      <div className="matchList"  >
-        <h3 className="titre">Choisir un match</h3>
-        <div className="matchListMatch">
-         {/* Calling the Match component. */}
-          <Match />
-          <Match />
-          <Match />
-          <Match />
-        </div>
-      </div>
-    </>
+   <>
+   </>
   )
 };

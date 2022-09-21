9adfe65c509e43441cd023c3d601302ad78fe1df
HaddoucheRym <rymmihoub@gmail.com>
2022-09-21 15:55:26 +0200

    feat: creat router in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import {GamblePage} from "./pages/GamblePage";
 import {HomePage} from "./pages/HomePage";
 import {ResultPage} from "./pages/ResultPage";
@@ -6,9 +7,30 @@ import {ResultPage} from "./pages/ResultPage";
 function App() {
   return (
     <>
-      {/* <HomePage /> */}
-      {/* <GamblePage /> */}
-      <ResultPage />
+     <Router>
+      <main>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/gaamble">About</Link>
+            </li>
+            <li>
+              <Link to="/result">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/"  component={HomePage} />
+        <Route path="/gamble" component={GamblePage} />
+        <Route path="/result" component={ResultPage} />
+      </main>
+    </Router>
+      {/* <HomePage />
+      <GamblePage />
+      <ResultPage /> */}
     </>
   );
 }

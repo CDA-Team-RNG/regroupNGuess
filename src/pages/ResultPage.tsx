b9628f071beeb21cb9cae04784ebe134fca7c625
Thibaut Clément <107027377+ThibautClement@users.noreply.github.com>
2022-09-27 10:10:56 +0200

    Style: Add style + assemble the result page.

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -3,6 +3,8 @@ import TeamResult from "../layout/TeamResult";
 
 import rogueLogo from "../assets/rogue-rebrand-removebg-2.png";
 import MADLogo from "../assets/Mad-lions-madrid-removebg-2.png";
+import MatchSelected from "../components/MatchSelected";
+import { Navbar } from "../layout/Navbar";
 
 const tempInfo = [
   {
@@ -12,6 +14,7 @@ const tempInfo = [
     betpoints: 1043,
     selected: true,
     wonMatch: true,
+    wonRound: 1,
   },
   {
     teamName: "MAD Lions",
@@ -20,25 +23,59 @@ const tempInfo = [
     betpoints: 3312,
     seleted: false,
     wonMatch: false,
+    wonRound: 0,
   },
 ];
 
+const data = {
+  gameImg: "https://www.pedagojeux.fr/wp-content/uploads/2019/11/1280x720_LoL.jpg",
+  game: "League of Legends",
+  league: "pupuLeague",
+  date: "24/06/22",
+  time: "20:00",
+  teamA: "Rogue",
+  teamB: "MAD Lions",
+};
+
 export const ResultPage = () => {
   return (
-    <section className="temp-team-result">
-      {tempInfo.map((element, index: number) => {
-        return (
-          <TeamResult
-            key={index}
-            teamName={element.teamName}
-            pic={element.image}
-            percentage={element.percentage}
-            betpoints={element.betpoints}
-            selected={element.selected}
-            isWon={element.wonMatch}
-          />
-        );
-      })}
-    </section>
+    <>
+      <Navbar/>
+      <MatchSelected data={data}/>
+      <section className="team-result">
+        <div>
+          <h3>Status du match :</h3>
+          <p>Terminé</p>
+          <p className="score"><span className={tempInfo[0].wonMatch == true ? "match-win" : "match-lost"}>{tempInfo[0].wonRound}</span><span> - </span><span className={tempInfo[1].wonMatch == true ? "match-win" : "match-lost"}>{tempInfo[1].wonRound}</span></p>
+        </div>
+      </section>
+      <section className="temp-team-result">
+        {tempInfo.map((element, index: number) => {
+          return (
+            <TeamResult
+              key={index}
+              teamName={element.teamName}
+              pic={element.image}
+              percentage={element.percentage}
+              betpoints={element.betpoints}
+              selected={element.selected}
+              isWon={element.wonMatch}
+            />
+          );
+        })}
+      </section>
+      <section className="gamble-result">
+        <p className="gamble-total-pts">Total points pariés : <span>4355</span></p>
+        <div className="gamble-result-box">
+          <div className="gamble-upper-result">
+            <p>Mise : <span>200</span></p>
+            <p>Gain : <span>+74</span></p>
+          </div>
+          <div className="gamble-lower-result">
+            <p>Total : <span>274</span></p>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
